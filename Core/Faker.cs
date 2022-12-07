f000e43ca2c9963c40c6612e9bc9656376570166
mc3Kast <k3sha2014@yandex.ru>
2022-12-07 13:33:14 +0300

    Add Faker class

---
+++ Core/Faker.cs
@@ -0,0 +1,40 @@
+﻿using Core.Generators;
+using Core.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Faker : IFaker
+    {
+        private readonly IEnumerable<IGenerator> _generators;
+        private readonly GeneratorContext _generatorContext;
+        private readonly CycleChecker _cycleChecker = new();
+
+        public Faker()
+        {
+            _generatorContext = new GeneratorContext(new Random(), this);
+            _generators = new List<IGenerator> { new BoolGenerator(), new ByteGenerator(),
+            new CharGenerator(), new DateTimeGenerator(), new DoubleGenerator(),
+            new FloatGenerator(), new IntGenerator(), new LongGenerator(), new ShortGenerator(),
+            new StringGenerator(), new ListGenerator()};
+        }
+
+        public T Create<T>() => (T)Create(typeof(T));
+
+        public object Create(Type type)
+        {
+            if (!_cycleChecker.AddType(type))
+                return null;
+
+            var obj = _generators.FirstOrDefault(g => g.CanGenerate(type), new ObjectGenerator())
+                .Generate(type, _generatorContext);
+
+            _cycleChecker.RemoveType(type);
+            return obj;
+        }
+    }
+}
---
+++ Core/Generators/ObjectGenerator.cs
@@ -0,0 +1,87 @@
+﻿using Core.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Generators
+{
+    public class ObjectGenerator : IGenerator
+    {
+        public object Generate(Type type, GeneratorContext context)
+        {
+            var obj = CreateObject(type, context);
+            return obj == null ? null : InitializeObject(obj, context);
+        }
+
+        public bool CanGenerate(Type type) => true;
+
+        private object? CreateObject(Type type, GeneratorContext context)
+        {
+            var constructors = type.GetConstructors()
+                .OrderByDescending(c => c.GetParameters().Length).ToList();
+
+            foreach (var constructorInfo in constructors)
+            {
+                try
+                {
+                    var args = constructorInfo.GetParameters()
+                        .Select(p => context.Faker.Create(p.ParameterType)).ToArray();
+                    return constructorInfo.Invoke(args);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            try
+            {
+                return Activator.CreateInstance(type);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return null;
+        }
+
+        private object InitializeObject(object obj, GeneratorContext context)
+        {
+            obj.GetType().GetProperties(BindingFlags.Public |
+                                        BindingFlags.Instance)
+                .Where(p => Equals(p.GetValue(obj), p.PropertyType.DefaultValue()))
+                .ForEach(property =>
+                {
+                    try
+                    {
+                        property.SetValue(obj, context.Faker.Create(property.PropertyType));
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                });
+
+            obj.GetType().GetFields()
+                .Where(f => !f.IsStatic)
+                .Where(f => Equals(f.GetValue(obj), f.FieldType.DefaultValue()))
+                .ForEach(field =>
+                {
+                    try
+                    {
+                        field.SetValue(obj, context.Faker.Create(field.FieldType));
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                });
+
+            return obj;
+        }
+    }
+}
